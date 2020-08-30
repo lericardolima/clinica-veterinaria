00174141ea391e4eb535c18187a8b877670e18b8
lericardolima <lericardolima@gmail.com>
2020-08-30 14:35:04 -0300

    Remover codigo de exemplo

--- api/example/routes/message.js
+++
@@ -1,6 +0,0 @@
-const controller = require('../controllers/message');
-
-module.exports = (app) => {
-    app.route('/message')
-    .get(controller.get);
-}
--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const express = require('express'),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser'),
   db = require('./config/database'),
-  routes = require('./api/example/routes/message');
+  routes = require('./api/routes');
 
+// Definição do body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Configuração de rotas
 routes(app);
+
+// Configuração do banco de dados
 db.config();
 
+// Start do servidor
 app.listen(port);
-
 console.log('TODO RESTful API server started on: ' + port);
 
 module.exports = app;
